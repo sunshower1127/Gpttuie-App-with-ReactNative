d1d50d1123c3b71319a8e4ee1f700e4905b1f4af
seng001127@soongsil.ac.kr <seng001127@soongsil.ac.kr>
2024-05-01 19:27:55 +0900

    0501 수업

--- pages/tabs/createRecipe.tsx
+++ pages/tabs/createRecipe.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import { View } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
 import { Text } from "react-native-paper";
+import { ingredients } from "../../assets/ingredients";
 
 export default function CreateRecipe() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState([]);
+  const [items, setItems] = useState(
+    ingredients.map((ingredient) => ({ label: ingredient, value: ingredient }))
+  );
+
   return (
     <View>
-      <Text variant={"headlineLarge"}>Create Recipe</Text>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        searchable={true}
+        multiple={true}
+        placeholder="재료를 검색해주세요."
+        maxHeight={400}
+        multipleText={value.join(", ")}
+      />
     </View>
   );
 }
