bf39745cd3bb235b379385fd3c17a55cd1337115
Sunwoo Kim <sunshower1127@gmail.com>
2024-05-13 00:45:55 +0900

    subscribes를 일단 create-post 보여주게 함

--- pages/tabs/subscribes.tsx
+++ pages/tabs/subscribes.tsx
@@ -1,5 +1,5 @@
 import WebView from "react-native-webview";
 
 export default function Subscribes() {
-  return <WebView source={{ uri: "https://gpttuie.web.app/" }} />;
+  return <WebView source={{ uri: "https://gpttuie.web.app/create-post" }} />;
 }
