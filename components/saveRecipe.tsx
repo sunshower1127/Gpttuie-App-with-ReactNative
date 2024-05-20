385e7bbaf46f8ba65c9707aa657a2dc995076937
오현 <127071470+jack3034936@users.noreply.github.com>
2024-05-20 15:43:54 +0900

    예비

---
+++ components/saveRecipe.tsx
@@ -0,0 +1,58 @@
+import RNFS from "react-native-fs";
+import React, { useState, useEffect } from "react";
+import { View, FlatList, Text } from "react-native";
+
+const FILE_PATH = `${RNFS.DocumentDirectoryPath}/flatListData.json`;
+
+const saveFlatListData = async (data) => {
+  try {
+    const jsonData = JSON.stringify(data);
+    await RNFS.writeFile(FILE_PATH, jsonData, "utf8");
+    console.log("FlatList data saved successfully");
+  } catch (error) {
+    console.error("Error saving FlatList data:", error);
+  }
+};
+
+const loadFlatListData = async () => {
+  try {
+    const jsonData = await RNFS.readFile(FILE_PATH, "utf8");
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error("Error loading FlatList data:", error);
+    return [];
+  }
+};
+
+const MyComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const loadedData = await loadFlatListData();
+      setData(loadedData);
+    };
+    loadData();
+  }, []);
+
+  const handleSaveData = () => {
+    saveFlatListData(data);
+  };
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Text>{item.title}</Text>
+    </View>
+  );
+
+  return (
+    <View>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <Button title="Save Data" onPress={handleSaveData} />
+    </View>
+  );
+};
---
+++ components/timer.tsx
@@ -0,0 +1,77 @@
+import { TimerPickerModal } from "react-native-timer-picker";
+import { LinearGradient } from "expo-linear-gradient"; // or `import LinearGradient from "react-native-linear-gradient"`
+import { Audio } from "expo-av"; // for audio feedback (click sound as you scroll)
+import * as Haptics from "expo-haptics"; // for haptic feedback
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+
+const Timer = () => {
+  const [showPicker, setShowPicker] = useState(false);
+  const [alarmString, setAlarmString] = useState<string | null>(null);
+
+  return (
+    <View
+      style={{
+        backgroundColor: "#514242",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 18, color: "#F1F1F1" }}>
+        {alarmString !== null ? "Alarm set for" : "No alarm set"}
+      </Text>
+      <TouchableOpacity activeOpacity={0.7} onPress={() => setShowPicker(true)}>
+        <View style={{ alignItems: "center" }}>
+          {alarmString !== null ? (
+            <Text style={{ color: "#F1F1F1", fontSize: 48 }}>
+              {alarmString}
+            </Text>
+          ) : null}
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => setShowPicker(true)}
+          >
+            <View style={{ marginTop: 30 }}>
+              <Text
+                style={{
+                  paddingVertical: 10,
+                  paddingHorizontal: 18,
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  fontSize: 16,
+                  overflow: "hidden",
+                  borderColor: "#C2C2C2",
+                  color: "#C2C2C2",
+                }}
+              >
+                Set Alarm 🔔
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+      <TimerPickerModal
+        visible={showPicker}
+        setIsVisible={setShowPicker}
+        onConfirm={(pickedDuration) => {
+          setAlarmString(formatTime(pickedDuration));
+          setShowPicker(false);
+        }}
+        modalTitle="Set Alarm"
+        onCancel={() => setShowPicker(false)}
+        closeOnOverlayPress
+        Audio={Audio}
+        LinearGradient={LinearGradient}
+        Haptics={Haptics}
+        styles={{
+          theme: "dark",
+        }}
+        modalProps={{
+          overlayOpacity: 0.2,
+        }}
+      />
+    </View>
+  );
+};
+
+export default Timer;
