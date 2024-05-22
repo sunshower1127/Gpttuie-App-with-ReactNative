8a92ccde0d24480bf4d924adbb3b46ca9a639cf8
오현 <127071470+jack3034936@users.noreply.github.com>
2024-05-22 15:57:49 +0900

    병합 안된내용 커밋

--- components/saveRecipe.tsx
+++ components/saveRecipe.tsx
@@ -2,10 +2,11 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Step, Recipe } from "../models/recipe";
 
 // Recipe[]를 저장합니다.
-const saveRecipes = async (recipes: Recipe[]) => {
+export const saveRecipes = async (recipes: Recipe[]) => {
   try {
     const jsonValue = JSON.stringify(recipes);
     await AsyncStorage.setItem("@recipes", jsonValue);
+    console.log("레시피 저장 완료");
   } catch (e) {
     // 저장 에러 처리
     console.error(e);
@@ -13,12 +14,30 @@ const saveRecipes = async (recipes: Recipe[]) => {
 };
 
 // 저장된 Recipe[]를 불러옵니다.
-const loadRecipes = async () => {
+export const loadRecipes = async () => {
   try {
     const jsonValue = await AsyncStorage.getItem("@recipes");
     return jsonValue != null ? JSON.parse(jsonValue) : null;
   } catch (e) {
     // 불러오기 에러 처리
+    console.error(e);
+  }
+};
+
+export const deleteRecipe = async (index: number) => {
+  try {
+    // 저장된 레시피를 불러옵니다.
+    const recipes = await loadRecipes();
+
+    if (recipes) {
+      // 특정 인덱스의 레시피를 삭제합니다.
+      recipes.splice(index, 1);
+
+      // 결과를 다시 저장합니다.
+      await saveRecipes(recipes);
+    }
+  } catch (e) {
+    // 삭제 에러 처리
     console.error(e);
   }
 };
